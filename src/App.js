e091b4438629b8437a6f48b11b2afbe5688c91d6
armanulhaq <armanulhaq10@gmail.com>
2023-06-17 12:28:07 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
 function App() {
+  //useState is a special function that helps us manage and update data in our app.
+  const [text, setText] = useState('');
+  const [wordCount, setWordCount] = useState(0);
+
+  const handleTextChange = (event) => {
+    const { value } = event.target;
+    setText(value);
+
+    // Count the number of words
+    const words = value.trim().split(/\s+/);
+    setWordCount(words.length);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="app">
+        <h1>Responsive Paragraph Word Counter</h1>
+        <textarea
+          className="text-area"
+          rows="6"
+          value={text}
+          onChange={handleTextChange}
+          placeholder="Type here..."
+        ></textarea>
+        <div className="word-count">Word Count: {wordCount}</div>
+      </div>
     </div>
   );
 }
